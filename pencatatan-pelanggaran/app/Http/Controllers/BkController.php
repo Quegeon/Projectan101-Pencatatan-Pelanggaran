482d033aabec83954729614c78a7a599eea5313d
Aldy Aditia Hidayat <111266754+Quegeon@users.noreply.github.com>
2023-11-24 14:00:08 +0700

    Update BkController.php

--- pencatatan-pelanggaran/app/Http/Controllers/BkController.php
+++ pencatatan-pelanggaran/app/Http/Controllers/BkController.php
@@ -7,26 +7,17 @@ use Illuminate\Http\Request;
 
 class BkController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
      $bk = Bk::all();
      return view('home.bk.index', compact(['bk']));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
      return view('home.bk.tambah');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
      Bk::create([
@@ -38,27 +29,13 @@ class BkController extends Controller
      ]);
      return redirect('/bk');
     }
-
-    /**
-     * Display the specified resource.
-     */
+    
     public function show($id)
     {
      $bk = Bk::find($id);
      return view('home.bk.edit',compact(['bk']));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
     public function update($id, Request $request)
     {
      $bk = Bk::find($id);
@@ -72,9 +49,6 @@ class BkController extends Controller
      return redirect('/bk');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy($id)
     {
      $bk = Bk::find($id);
