1299f9a357f33b6bd126d6d691b0ee13c3baee2e
Aldy Aditia Hidayat <111266754+Quegeon@users.noreply.github.com>
2023-11-24 14:10:48 +0700

    Update 2014_10_12_000000_create_users_table.php

--- pencatatan-pelanggaran/database/migrations/2014_10_12_000000_create_users_table.php
+++ pencatatan-pelanggaran/database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->string('password');
             $table->string('level');
             $table->string('foto');
-            $table->rememberToken();
             $table->timestamps();
         });
     }
