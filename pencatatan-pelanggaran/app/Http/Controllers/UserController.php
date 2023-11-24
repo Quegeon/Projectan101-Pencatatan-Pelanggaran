d223038e4374f7da8601ed3dd25accc2176ff633
Aldy Aditia Hidayat <111266754+Quegeon@users.noreply.github.com>
2023-11-24 14:12:10 +0700

    Update UserController.php

--- pencatatan-pelanggaran/app/Http/Controllers/UserController.php
+++ pencatatan-pelanggaran/app/Http/Controllers/UserController.php
@@ -7,26 +7,17 @@ use App\Http\Models\User;
 
 class UserController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
         $user = User::all();
         return view('home.user.index', compact('user'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
         return view('home.user.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
         //todo:tambah foto
@@ -40,26 +31,12 @@ class UserController extends Controller
         return redirect('/user');
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(string $id)
     {
         $user = User::find($id);
         return view('home.user.edit', compact(['user']));
     }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
+    
     public function update(Request $request, string $id)
     {
         //todo:tambah foto
@@ -74,9 +51,6 @@ class UserController extends Controller
         return redirect('/user');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(string $id)
     {
         $user = User::find($id);
