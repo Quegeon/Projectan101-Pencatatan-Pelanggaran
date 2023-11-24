0243c829374ae726831d3340f06930d09c623fde
unknown <raflisodri790@gmail.com>
2023-11-24 13:51:08 +0700

    update:siswa-feature

--- pencatatan-pelanggaran/app/Http/Controllers/Siswa/KelasController.php
+++ pencatatan-pelanggaran/app/Http/Controllers/Siswa/KelasController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Siswa;
 
 use App\Http\Controllers\Controller;
+use App\Models\Kelas;
 use Illuminate\Http\Request;
 
 class KelasController extends Controller
@@ -10,31 +11,46 @@ class KelasController extends Controller
     
     public function index()
     {
-        //
+        $kelas = Kelas::all();
+        return view('');
     }
 
     public function create()
     {
-        //
+      return view('');
     }
 
     public function store(Request $request)
     {
-        //
+        Kelas::create([
+            'id'=>$request->id,
+            'nama_kelas'=>$request->nama_kelas,
+            'jurusan'=>$request->jurusan,
+            $request->except(['_token']),
+        ]);
     }
 
     public function show(string $id)
     {
-        //
+        $kelas = Kelas::find($id);
+        return view('');
     }
 
     public function update(Request $request, string $id)
     {
-        //
+        $kelas = Kelas::find($id);
+        $kelas->update([
+            'id'=>$request->id,
+            'nama_kelas'=>$request->nama_kelas,
+            'jurusan'=>$request->jurusan,
+            $request->except(['_token']),
+        ]);
     }
 
     public function destroy(string $id)
     {
-        //
+        $kelas = Kelas::find($id);
+        $kelas->delete();
+        return redirect('');
     }
 }
--- pencatatan-pelanggaran/app/Http/Controllers/Siswa/SiswaController.php
+++ pencatatan-pelanggaran/app/Http/Controllers/Siswa/SiswaController.php
@@ -3,37 +3,63 @@
 namespace App\Http\Controllers\Siswa;
 
 use App\Http\Controllers\Controller;
+use App\Models\Siswa;
 use Illuminate\Http\Request;
 
 class SiswaController extends Controller
 {
     public function index()
     {
-        //
+        $siswa = Siswa::all();
+        return view('');
     }
 
     public function create()
     {
-        //
+        return view('');
     }
 
     public function store(Request $request)
     {
-        //
+        Siswa::create([
+            'nis'=>$request->nis,
+            'id_kelas'=>$request->id_kelas,
+            'nama'=>$request->nama,
+            'no_telp'=>$request->no_telp,
+            'alamat'=>$request->alamat,
+            'poin'=>$request->poin,
+            'status'=>$request->status,
+            $request->except(['_token']),
+        ]);
+        return redirect('');
     }
 
-    public function show(string $id)
+    public function show(string $nis)
     {
-        //
+        $siswa = Siswa::find($nis);
+        return view('');
     }
 
-    public function update(Request $request, string $id)
+    public function update(Request $request, string $nis)
     {
-        //
+        $siswa = Siswa::find($nis);
+        $siswa->update([
+            'nis'=>$request->nis,
+            'id_kelas'=>$request->id_kelas,
+            'nama'=>$request->nama,
+            'no_telp'=>$request->no_telp,
+            'alamat'=>$request->alamat,
+            'poin'=>$request->poin,
+            'status'=>$request->status,
+            $request->except(['_token']),
+        ]);
+        return redirect('');
     }
 
-    public function destroy(string $id)
+    public function destroy(string $nis)
     {
-        //
+        $siswa = Siswa::find($nis);
+        $siswa->delete();
+        return redirect('');
     }
 }
