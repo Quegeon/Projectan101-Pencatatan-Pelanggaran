988b620c337677278495d18031b6b512f79534ac
Aldy Aditia Hidayat <aschentegeon@gmail.com>
2023-11-29 15:40:34 +0700

    feat: petugas create pelanggaran

---
+++ pencatatan-pelanggaran/app/Http/Controllers/Pelanggaran/Bk/PelanggaranController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Pelanggaran\Bk;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PelanggaranController extends Controller
+{
+    //
+}
