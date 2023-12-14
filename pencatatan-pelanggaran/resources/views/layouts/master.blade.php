3f290cad0c844cf3790d4d6aa6f2264d9a84b357
Husein <126932455+maulanahusains@users.noreply.github.com>
2023-12-14 14:51:15 +0700

    fix: fixing redundant call

--- pencatatan-pelanggaran/resources/views/layouts/master.blade.php
+++ pencatatan-pelanggaran/resources/views/layouts/master.blade.php
@@ -267,9 +267,7 @@
 <!-- Azzara JS -->
 <script src="{{ asset('../../assets/js/ready.min.js') }}"></script>
 
-{{-- TODO: DIDOWNLOAD TRUS SIMPEN DI LOCAL, JADI GAPAKE CDN
-<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script> --}}
-
+{{-- TODO: DIDOWNLOAD TRUS SIMPEN DI LOCAL, JADI GAPAKE CDN  --}}
 <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 
 <script>
