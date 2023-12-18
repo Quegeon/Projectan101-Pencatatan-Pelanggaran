ac3f54eea6d174628d9583cef9116ac921415133
ghvvari <adunghivari@gmail.com>
2023-12-18 12:14:05 +0700

    coba

--- pencatatan-pelanggaran/database/migrations/2023_11_27_022819_create_pelanggarans_table.php
+++ pencatatan-pelanggaran/database/migrations/2023_11_27_022819_create_pelanggarans_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('pelanggarans', function (Blueprint $table) {
             $table->uuid('id')->primary();
+            $table->string('nis',15);
             $table->string('id_aturan')
                 ->nullable();
             $table->string('id_bk')
