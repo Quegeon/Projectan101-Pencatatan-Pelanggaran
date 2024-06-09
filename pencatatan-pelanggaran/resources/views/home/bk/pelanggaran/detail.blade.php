4b98ab19f9c97287933e8c9dd348965eea7607b9
maulana.husein <maulana.h@kabayan.id>
2024-06-09 09:48:25 +0700

    refactor: hide edit button when pelanggaran disabled

--- pencatatan-pelanggaran/resources/views/home/bk/pelanggaran/detail.blade.php
+++ pencatatan-pelanggaran/resources/views/home/bk/pelanggaran/detail.blade.php
@@ -158,7 +158,9 @@
                                     @if ($pelanggaran->status == 'Belum')
                                     <a href="{{ route('review.review', $pelanggaran->id) }}" class="btn btn-info"><i class="fa fa-edit mr-2"></i>Proses Pelanggaran</a>
                                     @else
-                                    <a href="{{ route('review.edit', $pelanggaran->id) }}" class="btn btn-info"><i class="fa fa-edit mr-2"></i>Edit Pelanggaran</a>
+                                        @if ($pelanggaran->is_active == 0)
+                                        <a href="{{ route('review.edit', $pelanggaran->id) }}" class="btn btn-info"><i class="fa fa-edit mr-2"></i>Edit Pelanggaran</a>
+                                        @endif
                                         @if ($pelanggaran->is_active == 0)
                                         <a href="" class="btn d-flex align-items-center btn-success btn-go-public">
                                             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="white" d="M12 4.5C7 4.5 2.73 7.61 1 12c1.73 4.39 6 7.5 11 7.5s9.27-3.11 11-7.5c-1.73-4.39-6-7.5-11-7.5M12 17c-2.76 0-5-2.24-5-5s2.24-5 5-5s5 2.24 5 5s-2.24 5-5 5m0-8c-1.66 0-3 1.34-3 3s1.34 3 3 3s3-1.34 3-3s-1.34-3-3-3"/></svg>
